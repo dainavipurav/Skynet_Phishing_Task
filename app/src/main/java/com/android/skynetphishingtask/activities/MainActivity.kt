50356de518fbea38a5f9f99fabad50916e3bdf56
dainavipurav <dainavipurav@gmail.com>
2020-10-15 00:48:30 +0530

    added deep linking soo that aap launched by clicking link from other apps
    changes manifest
    - added action view
    - added category Browsable and default
    - added data
    
    changes in main activity for incoming data

--- app/src/main/java/com/android/skynetphishingtask/activities/MainActivity.kt
+++ app/src/main/java/com/android/skynetphishingtask/activities/MainActivity.kt
@@ -1,36 +1,37 @@
 package com.android.skynetphishingtask.activities
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.android.skynetphishingtask.R
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mNavControllerMain: NavController
     lateinit var mBundle : Bundle
+    lateinit var mURL :String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mIntent : Intent = intent
-        val action : String = mIntent.action.toString()
-        val mType : String = mIntent.type.toString()
-
-        if (Intent.ACTION_SEND.equals(action) && mType != null) {
-            if ("text/plain".equals(mType)) {
-                val mSharedText: String = mIntent.getStringExtra(Intent.EXTRA_TEXT)
-                if (mSharedText != null) {
-                    val mNavController = this@MainActivity.findNavController(R.id.nav_host_fragment_container)
-                    mBundle = Bundle()
-                    mBundle.putString(getString(R.string.text_key_shared_text),mSharedText,)
-                    mNavController.setGraph(mNavController.graph,mBundle)
-                }
-            }
+        val mUri : Uri? = intent.data
+        if (mUri !=null){
+            /*var mParams : List<String> = mUri.pathSegments
+            var mId : String = mParams.get(mParams.size - 1)*/
+            mURL = mUri.toString()
         }
+        else{
+            mURL =""
+        }
+        val mNavController = this@MainActivity.findNavController(R.id.nav_host_fragment_container)
+        mBundle = Bundle()
+        mBundle.putString(getString(R.string.text_key_shared_text),mURL,)
+        mNavController.setGraph(mNavController.graph,mBundle)
     }
 }
